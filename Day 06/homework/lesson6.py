0b70832909a023f36acf57de3efb49e825f2417f
Saba20090303 <saba.bregva@gmail.com>
2024-02-23 21:41:57 +0400

    commit

--- Day 06/homework/lesson6.py
+++ Day 06/homework/lesson6.py
@@ -1,23 +1,15 @@
-num1 = int(input("enter your number"))
+#"35" '78'
+num1 = int(input("enter your number:"))
 num2 = int(input("enter your number"))
 num3 = int(input("enter your number"))
 
 print(num1 + num2 + num3)
 
-num1 = int(input("enter your number"))
-num2 = int(input("enter your number"))
-num3 = int(input("enter your number"))
+
 
 print(num1 - num2 - num3)
 
-num1 = int(input("enter your number"))
-num2 = int(input("enter your number"))
-num3 = int(input("enter your number"))
 
 print(num1 / num2 / num3)
-
-num1 = int(input("enter your number"))
-num2 = int(input("enter your number"))
-num3 = int(input("enter your number"))
 
 print(num1 * num2 * num3)
