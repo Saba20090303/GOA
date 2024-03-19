60f289f0c2c68eda8be87fb180f627ba8897606f
Saba20090303 <saba.bregva@gmail.com>
2024-03-19 18:41:51 +0400

    day 14

--- Day 09/classwork/lesson9.py
+++ Day 09/classwork/lesson9.py
@@ -9,9 +9,9 @@
 # for i in range(1,11):
 #     print(i*i)
 
-resalt = 0
-for i in range(1,11):
-    resalt = resalt + i
+# resalt = 0
+# for i in range(1,11):
+#     resalt = resalt + i
     
 
 # for i in range(1,21):
