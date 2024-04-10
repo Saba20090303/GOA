6febfa478ba8fd86d62c46ba5e8b60760d83e73a
Saba20090303 <saba.bregva@gmail.com>
2024-04-10 19:01:55 +0400

    Day 20

--- hakaton/hakaton3.py
+++
@@ -1,4 +0,0 @@
-list_1 = [63, 14, 29,23,56,32]
-sort = sorted(list_1)
-division = list_1[-1] - list_1[0]
-print(division)
--- hakaton/kahoot1.py
+++
@@ -1,4 +0,0 @@
-#შემოგდით ორი რიცხვი, გამოიტანეთ ამ ორი რიცხვის ჯაmi
-i = int(input("enter a first number:"))
-i1 = int(input("enter a second number:"))
-print(i + i1)
