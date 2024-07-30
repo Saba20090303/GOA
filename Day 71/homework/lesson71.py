29efd9d8432e3e776662c9fb5b43700c6b09e7f6
Saba20090303 <saba.bregva@gmail.com>
2024-07-30 12:04:19 +0400

    commit

--- Day 71/homework/lesson71.py
+++ Day 71/homework/lesson71.py
@@ -1,1 +1,16 @@
 #no homework
+from turtle import*
+speed(1)
+bgcolor("black")
+color("green")
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+
+exitonclick()
